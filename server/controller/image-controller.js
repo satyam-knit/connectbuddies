03c3c9f0c066a90bb481f1fd12e198b8c99dcb05
Satyam Sharma <98421460+satyam-knit@users.noreply.github.com>
2024-05-01 02:53:02 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = "http://localhost:8000";
+const url = "https://connectbuddies-1.onrender.com";
 
 
 export const uploadFile = async (request, response) => {
